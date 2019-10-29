72ce0f85982d799f16bc6bd15cf265263175e8c8
JannePernu <55883062+JannePernu@users.noreply.github.com>
2019-10-29 21:08:17 +0200

    Update DistanceCalculation.py

--- pan_tilt_tracking/DistanceCalculation.py
+++ pan_tilt_tracking/DistanceCalculation.py
@@ -1,6 +1,14 @@
 import tyres as ty
 
 def DistanceCalculation(w, dX):
+  float sPos = kit.servo[1].angle
+  
+  while sPos > 0:
+    ty.vasemmalle()
+			
+  while sPos < 0:
+    ty.oikealle()
+  
   while dX > w:
     ty.eteen()
    
